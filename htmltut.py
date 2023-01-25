b41b304865640803ee90508fbaceba7c3f15473d
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-01-25 10:26:00 +0300

    Create htmltut.py
    here file containing roadmap for HTML learning 
    with a description link here : https://roadmap.sh/frontend

---
+++ htmltut.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+in HTML learning: 
+  for below is the topical flow list:
+learn in range (0, 100 , on basics)
+for Writing Semantic HTML:
+if != Forms and Valeditions:
+and in conventions and Best Practices
+import accessibility.(HTML)
+import SEO.(Basics)
