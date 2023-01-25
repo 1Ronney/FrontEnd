590ced9c0098cebd6ec816f81c6f61234193f86d
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-01-25 10:17:53 +0300

    Create Internet.py

---
+++ Internet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import internet. datacenter. lastmile. backbone()
+import IP address.IPv6()
+import DNS. ("How it works")
+import Domain name ("what is the domain name")
+import hosting ("what is hosting")
